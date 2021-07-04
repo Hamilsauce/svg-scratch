40f7191aa232869182180fffda8909c47e5a166f
hamilsauce <jake@seahag.onmicrosoft.com>
2021-07-04 02:47:17 -0700

    swipe menu working

--- chrome-share-swipe/swipe.js
+++ chrome-share-swipe/swipe.js
@@ -5,10 +5,10 @@
 // 	game
 // } from './Game.js';
 
-		const expandMenu = (yPos) => {
-			menu.style.height = yPos
-			
-		}
+const expandMenu = (yPos) => {
+	menu.style.height = yPos
+
+}
 
 
 const menu = document.querySelector('.footer-menu')
@@ -35,10 +35,23 @@ const endTouch = (e) => {
 let shadowLength = 10;
 
 const moveTouch = (e) => {
-	const progressY = startY - e.touches[0].clientY
-	const translation = progressY > 0 ?
-		parseInt(-Math.abs(progressY)) :
-		parseInt(Math.abs(progressY))
+ const et = e.touches[0].clientY
+ if (startY - (et + 50) < 0) {
+	menu.classList.remove('expand')
+ 	
+ } else {
+	menu.classList.add('expand')
+ 	
+ }
+	const progressY = startY + e.touches[0].clientY
+	const targ = e.currentTarget;
+	targ.top = progressY
+	targ.height = '500px';
+	console.log(targ);
+	console.log(targ.height);
+	// const translation = progressY > 0 ?
+	// 	parseInt(Math.abs(progressY)) :
+	// 	parseInt(Math.abs(progressY))
 
 	// menu.querySelectorAll('.die-item').forEach(die => {
 	// 	die.style.boxShadow = `0px ${shadowLength - 8}px ${shadowLength}px 0px rgba(255, 255, 255, 0.7)`
@@ -47,16 +60,17 @@ const moveTouch = (e) => {
 }
 
 const startTouch = (e) => {
-		console.log(e.currentTarget);
+	// console.log(e.currentTarget);
 	const { touches } = e
+	// console.log(touches);
 	const menuStartHeight = parseInt(getComputedStyle(menu).height);
 	// Math.round(parseInt(getComputedStyle(menu).height.replace('px', '')) * 0.1)
-	console.log('h', menuStartHeight);
-	
+	// console.log('h', menuStartHeight);
+
 	if (touches && touches.length === 1) {
 		const touch = touches[0]
 		startY = touch.clientY
-		
+
 		// menu.querySelectorAll('.die-item').forEach(die => {
 		// 	die.style.boxShadow = `0px px ${5}px 0px rgba(255, 255, 255, 0.47)`
 		// })
@@ -67,6 +81,10 @@ const startTouch = (e) => {
 
 menu.addEventListener('touchstart', startTouch)
 
-const addSwipeAction = (el, dir, swipeFunc) => {
+const addSwipeAction = (el, dir, swipeFunc) => {}
 
-}
+
+document.querySelector('.footer-menu')
+.addEventListener('touchstart', e =>{
+	
+})
