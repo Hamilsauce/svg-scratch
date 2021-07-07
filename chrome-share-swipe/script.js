a89e4e1e3f99d2a7d193c525c35ef257d8830769
hamilsauce <jake@seahag.onmicrosoft.com>
2021-07-06 17:44:19 -0700

    good lookin swipe menu

--- chrome-share-swipe/script.js
+++ chrome-share-swipe/script.js
@@ -28,71 +28,41 @@ const svg = document.querySelector('svg');
 
 let showFooterMenu = false;
 
-// toggle.addEventListener('click', e => {
-// 	showFooterMenu = !showFooterMenu
-
-// 	if (showFooterMenu) {
-// 		footerMenu.classList.remove('hide')
-// 	} else {
-// 		footerMenu.classList.add('hide')
-// 	}
-// });
-
-
-
 const startDrag = (e) => {
 	const topBarCheck = e.path.some(el => el.id === 'menu-top-bar');
 	if (topBarCheck) {
-		// const targ = e.target;
-		const topBar = e.path.find(el => el.id === 'menu-top-bar');
 		const footer = e.path.find(el => el.id === 'footer-menu');
-		console.log(footer);
-		// const currentHeight = parseInt(getComputedStyle(footer).height)
-		topBar.addEventListener('touchmove', dragMenu, true)
-		topBar.addEventListener('touchend', stopDrag, true)
+		const topBar = e.path.find(el => el.id === 'menu-top-bar');
+		topBar.classList.add('pressed')
+
+		document.addEventListener('touchmove', dragMenu, true)
+		document.addEventListener('touchend', stopDrag, true)
 	} else {
-		console.log(e.path);
-		console.log('targ not bar');
 		return;
 	}
 };
 
 const stopDrag = (e) => {
-	console.log('stopDrag');
-	e.path
-		.find(el => el.id === 'menu-top-bar')
-		.removeEventListener('touchmove', dragMenu, true)
+	e.path.find(el => el.id === 'menu-top-bar')
+		.classList.remove('pressed')
 
-	// e.target.removeEventListener('touchmove', dragMenu)
+	document.removeEventListener('touchmove', dragMenu, true)
 }
 
-
 const dragMenu = (e) => {
-	console.log(e);
-	// if (e.target.id === 'menu-top-bar') {
+	const app = document.querySelector('.app');
 	const topBar = e.path.find(el => el.id === 'menu-top-bar');
 	const footer = e.path.find(el => el.id === 'footer-menu');
 	const currentHeight = parseInt(getComputedStyle(footer).height)
-	
-	const touch = e.touches[0].clientY
-	const finTouch = e.changedTouches[0].clientY
-	console.log(startTouch, finTouch);
-	console.log('fin t', e.changedTouches);
-	if (touch <  (currentHeight - 5) ||touch >  (currentHeight + 5)) {
-		console.log('scrollin');
-	} else if (startY > (finishingTouch + swipeMin)) {}
+	const appHeight = parseInt(getComputedStyle(app).height)
+	const touch = e.changedTouches[0].pageY
 
-	const dragPosition = Math.floor(e.targetTouches[0].clientY);
-	footer.style.height = `${dragPosition}px`
-	console.log('curr height', Math.floor(currentHeight));
-	console.log('targ touches', Math.floor(e.targetTouches[0].clientY));
-	console.log('touches', Math.floor(e.touches[0].clientY));
-	console.log(getComputedStyle(footer).height);
-	// } else {
-	// re/turn;
-	// }
+	if ((touch > (appHeight - 130))) {} else if (touch <= 130) {
+		footer.style.height = `${currentHeight}px`
+	} else {
+		footer.style.height = `${(appHeight - touch) + 20}px`
+	}
+	topBar.focused = false
 }
 
-
-
-document.querySelector('#menu-top-bar').addEventListener('touchstart', startDrag)
+document.addEventListener('touchstart', startDrag)
